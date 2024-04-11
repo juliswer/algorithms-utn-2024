2bd1a139f27ef4251e136c411ba4fbaba809faed
Julian Swerdlin <julianswer36@gmail.com>
2024-04-10 21:18:44 -0300

    feat: exercise 10

--- practica1.cpp
+++ practica1.cpp
@@ -173,9 +173,62 @@ void exerciseNine() {
     std::cout << "Due to your violations you will have to pay a fine of: $" << penaltyFee << std::endl;
 }
 
+// Exercise 10
+
+struct Report {
+    // < 1520
+    int categoryOneCounter;
+    // >= 1520 && < 2780
+    int categoryTwoCounter;
+    // >= 2780 && < 5999
+    int categoryThreeCounter;
+    // >= 5999
+    int categoryFourCounter;
+};
+
+void exerciseTen(const float* arrOfPayments, int arrLength) {
+    Report report = {};
+    report.categoryOneCounter = 0;
+    report.categoryTwoCounter = 0;
+    report.categoryThreeCounter = 0;
+    report.categoryFourCounter = 0;
+
+    // arrLength - 1 to prevent handling the zero.
+    for (int i = 0; i < arrLength - 1; ++i) {
+        if(arrOfPayments[i] < 1520)
+            report.categoryOneCounter++;
+
+        if(arrOfPayments[i] >= 1520 && arrOfPayments[i] < 2780)
+            report.categoryTwoCounter++;
+
+        if(arrOfPayments[i] >= 2780 && arrOfPayments[i] < 5999)
+            report.categoryThreeCounter++;
+
+        if(arrOfPayments[i] >= 5999)
+            report.categoryFourCounter++;
+    }
+
+    std::cout << "Analysis Concluded!" << '\n';
+
+    std::cout << std::endl;
+    std::cout << "Employees earning less than $1.520: " << report.categoryOneCounter << '\n';
+    std::cout << std::endl;
+    std::cout << "Employees earning more than than $1.520 but less than $2.780: " << report.categoryTwoCounter << '\n';
+    std::cout << std::endl;
+    std::cout << "Employees earning more than $2.780 but less than $5.999: " << report.categoryThreeCounter << '\n';
+    std::cout << std::endl;
+    std::cout << "Employees earning more than $5.999: " << report.categoryFourCounter << '\n';
+
+    std::cout << std::endl;
+}
+
 
 int main() {
-    exerciseNine();
+    float arrOfPayments[] = {1519, 1518, 1517, 1516, 0};
+    int arrOfPaymentsLength = sizeof(arrOfPayments) / sizeof(arrOfPayments[0]);
+
+    exerciseTen(arrOfPayments, arrOfPaymentsLength);
+
     return 0;
 }
 
